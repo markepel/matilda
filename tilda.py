2a115792513542a867874990c1e21f71ee9aabf5
Epel Mark <epelmark@gmail.com>
2019-12-27 23:19:37 +0200

    delete import

--- tilda.py
+++ tilda.py
@@ -10,6 +10,49 @@ import numpy as np
 
 app = Flask(__name__)
 
+def image_generator(stream):
+    bytes = b''
+    try:
+        while True:
+            bytes += stream.read(1024)
+            a = bytes.find(b'\xff\xd8')
+            b = bytes.find(b'\xff\xd9')
+            if a != -1 and b != -1:
+                jpg = bytes[a:b+2]
+                bytes = bytes[b+2:]
+                i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+                #cv2.imwrite('image{}.jpg'.format(counter), i)
+                #counter += 1
+                (flag, encodedImage) = cv2.imencode(".jpg", i)
+                yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+                # print('flag {}'.format(flag))
+                # print('encodedImage {}'.format(encodedImage))
+                # print(type(i))
+                # print('i {}, type {}'.format(i, type(i)))
+                # cv2.imshow('i', i)
+            if not bytes:
+                raise Exception('No bytes')
+        stream.close()
+        print('stream closed')
+    except Exception as e:
+        print('Exception')
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_tb(exc_traceback, limit=1, file=sys.stdout)
+        traceback.print_exc(limit=2, file=sys.stdout)
+
+        try:
+            connection.close()
+            #server_socket.close()
+        except:
+            print('Error in except server connection.close()')
+        print('connection closed, listening')
+        #server_socket.listen(0)
+        print('after listen')
+        #connection = server_socket.accept()[0].makefile('rb')
+        print('new connection')
+    
+
+
 @app.route("/video_feed")
 def video_feed():
     # return 'Hello, World!'
@@ -28,47 +71,7 @@ print('connection accepted')
 app.run(host='0.0.0.0', port=5000, debug=True,
         threaded=True, use_reloader=False)
 
-def image_generator():
-    bytes = b''
-    try:
-        while True:
-            bytes += connection.read(1024)
-            a = bytes.find(b'\xff\xd8')
-            b = bytes.find(b'\xff\xd9')
-            if a != -1 and b != -1:
-                jpg = bytes[a:b+2]
-                bytes = bytes[b+2:]
-                i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-                #cv2.imwrite('image{}.jpg'.format(counter), i)
-                #counter += 1
-                (flag, encodedImage) = cv2.imencode(".jpg", i)
-                yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
-                # print('flag {}'.format(flag))
-                # print('encodedImage {}'.format(encodedImage))
-                # print(type(i))
-                # print('i {}, type {}'.format(i, type(i)))
-                # cv2.imshow('i', i)
-            if not bytes:
-                raise Exception('No bytes')
-        connection.close()
-        print('connection closed')
-    except Exception as e:
-        print('Exception')
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_tb(exc_traceback, limit=1, file=sys.stdout)
-        traceback.print_exc(limit=2, file=sys.stdout)
 
-        try:
-            connection.close()
-            #server_socket.close()
-        except:
-            print('Error in except server connection.close()')
-        print('connection closed, listening')
-        #server_socket.listen(0)
-        print('after listen')
-        #connection = server_socket.accept()[0].makefile('rb')
-        print('new connection')
-    
 
 #while True:
 print('Listening...')
