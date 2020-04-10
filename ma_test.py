cb1e09afc2e99470b58420f4616fcbc528b50a40
Epel Mark <epelmark@gmail.com>
2020-04-10 14:05:38 +0300

    fix syntx

--- ma_test.py
+++ ma_test.py
@@ -2,15 +2,13 @@ import picamera
 import time
 
 
-print('Starting')
-
-    print('a')
-    with picamera.PiCamera() as camera:
-        print('b')
-        camera.start_preview()
-        print('c')
-        time.sleep(2)
-        print('d')
-        for filename in camera.capture_continuous(f'img{counter}.jpg'):
-            print(f'Captured {filename}')
-            time.sleep(4)
+print('a')
+with picamera.PiCamera() as camera:
+    print('b')
+    camera.start_preview()
+    print('c')
+    time.sleep(2)
+    print('d')
+    for filename in camera.capture_continuous(f'img{counter}.jpg'):
+        print(f'Captured {filename}')
+        time.sleep(4)
