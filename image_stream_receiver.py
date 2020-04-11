734ef5ea3afa983ea427818872fb438a41f146c6
Epel Mark <epelmark@gmail.com>
2020-04-11 16:24:10 +0300

    image stream receiver add flask

--- image_stream_receiver.py
+++ image_stream_receiver.py
@@ -2,6 +2,9 @@ import io
 import socket
 import struct
 from PIL import Image
+from flask import Response
+from flask import Flask
+from flask import render_template
 
 server_socket = socket.socket()
 server_socket.bind(('0.0.0.0', 8008))
@@ -9,27 +12,37 @@ server_socket.listen(0)
 connection = server_socket.accept()[0].makefile('rb')
 print('connection accepted')
 
-try:
-    while True:
-        # Read the length of the image as a 32-bit unsigned int. If the
-        # length is zero, quit the loop
-        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
-        if not image_len:
-            break
-        # Construct a stream to hold the image data and read the image
-        # data from the connection
-        image_stream = io.BytesIO()
-        image_stream.write(connection.read(image_len))
-        # Rewind the stream, open it as an image with PIL and do some
-        # processing on it
-        image_stream.seek(0)
-        image = Image.open(image_stream)
-        print('Image is %dx%d' % image.size)
-        image.verify()
-        print('Image is verified')
-finally:
-    connection.close()
-    server_socket.close()
+
+
+def image_generator():
+  try:
+      while True:
+          image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+          if not image_len:
+              break
+          image_stream = io.BytesIO()
+          image_stream.write(connection.read(image_len))
+          image_stream.seek(0)
+          yield (b'--frame\r\n'
+                 b'Content-Type: image/jpeg\r\n\r\n' + image_stream + b'\r\n')
+          print('after yield image')
+          # image = Image.open(image_stream)
+          # print('Image is %dx%d' % image.size)
+          # image.verify()
+          # print('Image is verified')
+  finally:
+      connection.close()
+      server_socket.close()
+
+
+
+app = Flask(__name__)
+@app.route("/video_feed")
+def video_feed():
+    # return 'Hello, World!'
+    print('Starting video feeding')
+    return Response(image_generator(),
+        mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 
 
