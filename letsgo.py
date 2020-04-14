33122c046336e8735e73aaadaf5a440f55186a88
Epel Mark <epelmark@gmail.com>
2020-04-14 21:19:31 +0300

    syntax

--- letsgo.py
+++ letsgo.py
@@ -15,7 +15,7 @@ from image_generator import ImageGenerator
 
 
 def set_logger():
-    FORMAT = '%(asctime)-15s %(user)-8s %(message)s'
+    FORMAT = '%(asctime)-15s %(message)s'
     logging.basicConfig(format=FORMAT)
     logger.info('logger initialized')
 
