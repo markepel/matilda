1d6139f4ecb02e455d1f5e79d80804ebcf7b21ca
Epel Mark <epelmark@gmail.com>
2020-04-29 14:15:58 +0300

    test send bytesarray

--- motion_detection_preprocessor.py
+++ motion_detection_preprocessor.py
@@ -42,7 +42,8 @@ class MotionDetectionProcessor():
         (flag, encodedImage) = cv2.imencode(".jpg", image)
         self.motion_detector.update(gray)
         if motion_detected:
-            self.thread_executor.submit(send_image, bytearray(encodedImage))
+            send_image(bytearray(encodedImage))
+            # self.thread_executor.submit(send_image, bytearray(encodedImage))
         return bytearray(encodedImage)
 
 def send_image(image):
